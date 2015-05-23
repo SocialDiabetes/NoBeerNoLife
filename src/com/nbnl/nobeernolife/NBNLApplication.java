b3e9d6e7456fe72ca98920bc63decdb41a823a48
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-05-23 13:56:36 +0900

    画像読み込み用Universal Image Loaderの設定

--- src/com/nbnl/nobeernolife/NBNLApplication.java
+++ src/com/nbnl/nobeernolife/NBNLApplication.java
@@ -1,11 +1,20 @@
 package com.nbnl.nobeernolife;
 
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
 import android.app.Application;
 
 public class NBNLApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		ImageLoaderConfiguration.Builder imageLoaderBuilder = new ImageLoaderConfiguration.Builder(getApplicationContext());
+		imageLoaderBuilder.memoryCache(new LruMemoryCache(8 * 1024 * 1024));
+		imageLoaderBuilder.memoryCacheSize(8 * 1024 * 1024);
+		ImageLoaderConfiguration config = imageLoaderBuilder.build();
+		ImageLoader.getInstance().init(config);
 	}
 
 	@Override
