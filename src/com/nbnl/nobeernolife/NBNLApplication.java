440dab3d78de809bb7f485c2d35a79c3aa363fe0
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-05-23 13:51:37 +0900

    よくよく考えるとSQLite使っちゃうと多少ハード依存になっちゃって都合が悪いのでSQLiteを使うのはやめる

--- src/com/nbnl/nobeernolife/NBNLApplication.java
+++ src/com/nbnl/nobeernolife/NBNLApplication.java
@@ -2,17 +2,10 @@ package com.nbnl.nobeernolife;
 
 import android.app.Application;
 
-import com.activeandroid.ActiveAndroid;
-import com.activeandroid.Configuration;
-
 public class NBNLApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		Configuration.Builder activeAndroidBuilder = new Configuration.Builder(getBaseContext());
-		activeAndroidBuilder.setDatabaseName("nbnl.db");
-		activeAndroidBuilder.setDatabaseVersion(1);
-		ActiveAndroid.initialize(activeAndroidBuilder.create(), true);
 	}
 
 	@Override
--- src/com/nbnl/nobeernolife/VoiceAudioModel.java
+++ src/com/nbnl/nobeernolife/VoiceAudioModel.java
@@ -1,19 +1,8 @@
 package com.nbnl.nobeernolife;
 
-import com.activeandroid.Model;
-import com.activeandroid.annotation.Column;
-
-public class VoiceAudioModel extends Model {
-	@Column(name = "item_id", notNull = true)
+public class VoiceAudioModel{
 	public long id;
-	@Column(name = "category", notNull = true)
 	public int category = 0;
-	@Column(name = "filepath", notNull = true)
 	public String filepath;
-	@Column(name = "voice_text", notNull = true)
 	public String voice_text;
-	@Column(name = "updated_at", notNull = true)
-	public long updated_at = System.currentTimeMillis();
-	@Column(name = "created_at", notNull = true)
-	public long created_at = System.currentTimeMillis();
 }
